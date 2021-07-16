dd33bad528c0f188f934fc2e6bd6afee56ba62cf
Akshay <flammable.akshay@gmail.com>
2021-07-16 19:50:57 +0200

    Update config_ctc.py

--- code/BERT_NER/utils_ctc/config_ctc.py
+++ code/BERT_NER/utils_ctc/config_ctc.py
@@ -1,10 +1,10 @@
 parameters_ctc = {}
 
 
-parameters_ctc['RESOURCES_base_directory'] = "/content/data_ctc"
+parameters_ctc['RESOURCES_base_directory'] = "/content/drive/MyDrive/StackOverflowNER/data_ctc"
 
-parameters_ctc['train_file']="/content/data_ctc/train.tsv"
-parameters_ctc['test_file']="/content/data_ctc/test.tsv"
+parameters_ctc['train_file']="/content/drive/MyDrive/StackOverflowNER/data_ctc/train.tsv"
+parameters_ctc['test_file']="/content/drive/MyDrive/StackOverflowNER/data_ctc/test.tsv"
 
 
 parameters_ctc['LR']=0.0015
